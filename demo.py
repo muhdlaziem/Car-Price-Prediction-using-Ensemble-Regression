9c84d12c7bda69f94ae55fb011c838f24745a685
Laziem <muhdlaziem97@gmail.com>
2019-05-05 12:03:52 +0800

    MLR

---
+++ demo.py
@@ -0,0 +1,16 @@
+from numpy import *
+def run():
+    points = genfromtext('data.csv', delimiter=',')
+    #hyperparameters
+    learning_rate = 0.0001
+
+    #y = mx + b
+    initial_b = 0
+    initial_m = 0
+    num_iterations = 1000
+    print
+
+
+
+if__name__ = '__main__':
+    run()
